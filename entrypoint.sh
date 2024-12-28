4c81a071411549264c0b5b5c8ae3ea82e0de872f
Geobellz22 <106580682+Geobellz22@users.noreply.github.com>
2024-12-28 23:26:03 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,5 @@ python manage.py migrate
 # Start Daphne server
 exec daphne -b 0.0.0.0 -p 8000 jwt_auth.asgi:application
  
- The  entrypoint.sh  script is responsible for applying database migrations and starting the Daphne server. 
- The  Dockerfile  is used to build the Docker image.
+ # The  entrypoint.sh  script is responsible for applying database migrations and starting the Daphne server. 
+ # The  Dockerfile  is used to build the Docker image.
