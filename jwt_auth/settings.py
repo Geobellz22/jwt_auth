3c4cf818788ffaabf0923eb0901ee55dbcc0fc9d
Geobellz22 <106580682+Geobellz22@users.noreply.github.com>
2024-12-29 16:26:22 +0100

    Update settings.py

--- jwt_auth/settings.py
+++ jwt_auth/settings.py
@@ -102,7 +102,7 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             'hosts': [
-                (config('REDIS_HOST', default='127.0.0.1'), config('REDIS_PORT', default=6379, cast=int))
+                config('REDIS_URL')
             ],
         },
     },
