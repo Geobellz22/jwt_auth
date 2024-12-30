366d3456c079c91cc396e31ae320b5e9b946b401
Geobellz22 <106580682+Geobellz22@users.noreply.github.com>
2024-12-30 16:44:26 +0100

    Update asgi.py

--- jwt_auth/asgi.py
+++ jwt_auth/asgi.py
@@ -11,17 +11,13 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+os.environ['DJANGO_SETTINGS_MODULE']='jwt_auth.settings'
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jwt_auth.settings')
+django_asgi_application=get_asgi_application()
 from Chat import routing
-
-# Set the Django settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jwt_auth.settings')
-
-# Get the ASGI application
-django_asgi_application = get_asgi_application()
-
 # Define the ASGI application
 application = ProtocolTypeRouter({
-    "http": django_asgi_application,  # Correctly setup HTTP handler
+    "http": django_asgi_application,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
