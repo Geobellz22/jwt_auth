77df7dc755c5aaac32b11b44e813c6043ec973a7
Geobellz22 <106580682+Geobellz22@users.noreply.github.com>
2024-12-30 22:53:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# Set the DJANGO_SETTINGS_MODULE environment variable
-export DJANGO_SETTINGS_MODULE=jwt_auth.settings
+# Exit immediately if a command exits with a non-zero status
+set -e
 
 # Apply database migrations
-python manage.py makemigrations
-python manage.py migrate
+echo "Applying database migrations..."
+python manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
 
 # Start Daphne server
+echo "Starting Daphne server..."
 exec daphne -b 0.0.0.0 -p 8000 jwt_auth.asgi:application
