5d8b26ca7944c71ec2c8e8a5e2fd1e334abb9dfb
Geobellz22 <106580682+Geobellz22@users.noreply.github.com>
2025-01-20 22:30:40 +0100

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import register_user, confirm_email, CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView  # Correct import for TokenRefreshView
 
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('confirm-email/', confirm_email, name='confirm_email'),
-    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # Ensure CustomTokenObtainPairView is correctly defined
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),  # Custom TokenObtainPairView
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # TokenRefreshView import corrected
 ]
