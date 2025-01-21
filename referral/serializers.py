816b0648f973a0311f5fb0674819c89250047cbf
Geobellz22 <106580682+Geobellz22@users.noreply.github.com>
2025-01-21 23:07:14 +0100

    Update serializers.py

--- referral/serializers.py
+++ referral/serializers.py
@@ -1,18 +1,24 @@
-# referral/serializers.py
 from rest_framework import serializers
 from .models import Referral, ReferralReward
 
+
 class ReferralSerializer(serializers.ModelSerializer):
-    referred_by_username = serializers.CharField(source='referred_by.username', read_only=True)
+    referred_by_username = serializers.CharField(
+        source='referred_by.username', read_only=True
+    )
 
     class Meta:
         model = Referral
         fields = ['user', 'referral_code', 'referred_by', 'referred_by_username', 'created_at']
-        read_only_fields = ['created_at']
+        read_only_fields = ['referral_code', 'created_at']
+
 
 class ReferralRewardSerializer(serializers.ModelSerializer):
-    referral_user = serializers.CharField(source='referral.user.username', read_only=True)
+    referral_user = serializers.CharField(
+        source='referral.user.username', read_only=True
+    )
 
     class Meta:
         model = ReferralReward
         fields = ['referral', 'referral_user', 'level', 'reward_percentage']
+        read_only_fields = ['reward_percentage']  # Rewards are predefined
