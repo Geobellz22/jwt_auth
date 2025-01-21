819d90f3e891523af547affafb8c25ba32cbd39c
Geobellz22 <106580682+Geobellz22@users.noreply.github.com>
2025-01-21 22:50:54 +0100

    Update serializers.py

--- ReferralLinks/serializers.py
+++ ReferralLinks/serializers.py
@@ -1,14 +1,13 @@
 from rest_framework import serializers
 from .models import ReferralLink
 
-class ReferralLink(serializers.ModelSerializer):
+class ReferralLinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReferralLink
         fields = ['id', 'user', 'referral_link', 'referral_code', 'created_at', 'reward_granted']
         read_only_fields = ('referral_code', 'created_at', 'reward_granted')
-        
-        def validate_referred_user(self, value):
-            if value:
-                return serializers.ValidationError("You can't manually set referred user.")
-            return value 
-        
+
+    def validate_referred_user(self, value):
+        if value:
+            raise serializers.ValidationError("You can't manually set referred user.")
+        return value
