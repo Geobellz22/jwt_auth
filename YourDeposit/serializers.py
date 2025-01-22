e503451ce2e08e39cf2f78b7bba2d1fd75ecb375
Geobellz22 <106580682+Geobellz22@users.noreply.github.com>
2025-01-23 00:27:26 +0100

    Update serializers.py

--- YourDeposit/serializers.py
+++ YourDeposit/serializers.py
@@ -4,4 +4,4 @@ from .models import Deposit
 class YourDepositSerializer(serializers.ModelSerializer):
     class Meta:
         model = Deposit
-        fields = ['user', 'amount', 'wallet_type', 'wallet_address', 'status', 'transaction_id', 'created_at    ' ]
+        fields = ['user', 'amount', 'wallet_type', 'wallet_address', 'status', 'transaction_id', 'created_at']
