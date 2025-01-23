5d1fd09d593269b3af52e840207140aa1dae347f
Geobellz22 <106580682+Geobellz22@users.noreply.github.com>
2025-01-23 01:01:19 +0100

    Update urls.py

--- referral/urls.py
+++ referral/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import create_referral, get_referral_rewards
+from .views import CreateReferralView, GetReferralRewardsView
 
 urlpatterns = [
-    path('referral/', create_referral, name='create-referral'),  # View to create a referral
-    path('referral/reward/<int:user_id>/', get_referral_rewards, name='get-referral-rewards'),  # View to get referral rewards by user ID
+    path('referral/', CreateReferralView.as_view(), name='create-referral'),  # View to create a referral
+    path('referral/reward/<int:user_id>/', GetReferralRewardsView.as_view(), name='get-referral-rewards'),  # View to get referral rewards by user ID
 ]
