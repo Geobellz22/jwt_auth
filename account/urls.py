ae4b75edb3341473fa1fc8b682f57f0a8789cb82
Geobellz22 <106580682+Geobellz22@users.noreply.github.com>
2025-01-24 18:55:34 +0100

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RegisterView, ConfirmmailView, CustomTokenObtainPairView, CustomTokenRefreshView
+from .views import RegisterView, ConfirmMailView, CustomTokenObtainPairView, CustomTokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('confirm-email/', ConfirmmailView.as_view(), name='confirm_email'),
+    path('confirm-email/', ConfirmMailView.as_view(), name='confirm_email'),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 ]
