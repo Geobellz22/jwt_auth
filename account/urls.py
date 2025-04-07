6ef7a3ca99d97f0c2bce316a92fe7ab4935d2530
Geobellz22 <st377126@gmail.com>
2025-04-07 22:32:44 +0100

    registration successful with user verified

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView,  LoginView
+from .views import RegisterView, ConfirmMailView, CustomTokenObtainPairView, CustomTokenRefreshView, LoginView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    #path('confirm-email/', ConfirmMailView.as_view(), name='confirm_email'),
-    #path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('confirm-email/', ConfirmMailView.as_view(), name='confirm_email'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('login/', LoginView.as_view(), name='login'),
 ]
